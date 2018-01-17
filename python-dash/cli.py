b41526c915a416a4fd0caaf47d549ad04025f3ff
JamesDBowen <JamesDBowen@gmail.com>
2018-01-17 10:53:44 -0500

    Update cli.py

--- python-dash/cli.py
+++ python-dash/cli.py
@@ -8,7 +8,7 @@ class DashCLI():
   __init__(self):
     print "Initializing Dash CLI"
   
-  def Call(self, command, **args, ***kwargs):
+  def call(self, command, **args, ***kwargs):
     """
     Stub for now
     """
