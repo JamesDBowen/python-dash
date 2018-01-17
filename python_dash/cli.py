b70e58812b269f78e1d542a871aef7d9da86793b
JamesDBowen <JamesDBowen@gmail.com>
2018-01-17 11:13:59 -0500

    Update cli.py

--- python_dash/cli.py
+++ python_dash/cli.py
@@ -5,7 +5,7 @@ A wrapper for passing commands to the Dash CLI
 import os
 
 class DashCLI():
-  __init__(self):
+  def __init__(self):
     print "Initializing Dash CLI"
   
   def call(self, command, **args, ***kwargs):
