168206fae82c655079295ce5533bf79e8e945c9a
JamesDBowen <JamesDBowen@gmail.com>
2018-01-17 11:15:40 -0500

    Create cli.py

---
+++ dash/cli.py
@@ -0,0 +1,16 @@
+"""
+A wrapper for passing commands to the Dash CLI
+"""
+
+import os
+
+class DashCLI():
+  def __init__(self):
+    print "Initializing Dash CLI"
+  
+  def call(self, command, **args, ***kwargs):
+    """
+    Stub for now
+    """
+    return
+    
