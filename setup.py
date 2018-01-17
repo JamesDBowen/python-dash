3320df154e4aa77d9b103492deab5c5166511629
JamesDBowen <JamesDBowen@gmail.com>
2018-01-17 09:44:00 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+distutils/setuptools install script.
+"""
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+ROOT = os.path.dirname(__file__)
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+
+
+requires = [
+]
+
+
+def get_version():
+    init = open(os.path.join(ROOT, 'python-dash', '__init__.py')).read()
+    return VERSION_RE.search(init).group(1)
+
+
+setup(
+    name='python-dash',
+    version=get_version(),
+    description='A wrapper for the Dash CLI in Python',
+    long_description=open('README.rst').read(),
+    author='James Bowen',
+    url='https://github.com/JamesDBowen/python-dash',
+    scripts=[],
+    packages=find_packages(exclude=['tests*']),
+    install_requires=requires,
+    license="MIT",
+    classifiers=[
+        'Development Status :: 1 - Planning Development Status',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: MIT',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
