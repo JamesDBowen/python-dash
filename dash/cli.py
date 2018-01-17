da12295c3a147fcaac2eccc22f3da07db00afc55
JamesDBowen <JamesDBowen@gmail.com>
2018-01-17 11:24:09 -0500

    Update cli.py

--- dash/cli.py
+++ dash/cli.py
@@ -2,7 +2,7 @@
 A wrapper for passing commands to the Dash CLI
 """
 
-import os
+import subprocess
 
 class DashCLI():
   def __init__(self):
@@ -12,5 +12,8 @@ class DashCLI():
     """
     Stub for now
     """
+    command_string = "dash-cli "
+    command_string += command
+    subprocess.checkoutput(command_string)
     return
     
